fe0dec7d41c2ff0fcfbebdd88a10b3c299006474
Dipen Ved <vedipen@gmail.com>
2017-01-10 00:31:01 +0530

    Update Project

---
+++ src/lib/local/FaceAnalyser/include/GazeEstimation.h
@@ -0,0 +1,15 @@
+
+#ifndef __GAZEESTIMATION_h_
+#define __GAZEESTIMATION_h_
+
+#include "opencv2/core/core.hpp"
+#include "LandmarkCoreIncludes.h"
+
+namespace FaceAnalysis
+{
+
+	void EstimateGaze(const LandmarkDetector::CLNF& clnf_model, cv::Point3f& gaze_absolute, float fx, float fy, float cx, float cy, bool left_eye);
+	void DrawGaze(cv::Mat img, const LandmarkDetector::CLNF& clnf_model, cv::Point3f gazeVecAxisLeft, cv::Point3f gazeVecAxisRight, float fx, float fy, float cx, float cy);
+
+}
+#endif
